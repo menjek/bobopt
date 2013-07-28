4f54adab40541111ad5520b60ac6c4edcfffb9b6
menjek <menjek@gmail.com>
2013-07-28 21:28:12 +0200

    + base for analysis of code complexity + creation of complexity tree + make_unique based on paper from STL

---
+++ bobopt_utils.hpp
@@ -0,0 +1,57 @@
+#ifndef BOBOPT_UTILS_HPP_GUARD_
+#define BOBOPT_UTILS_HPP_GUARD_
+
+#include <bobopt_language.hpp>
+
+#include <memory>
+#include <type_traits>
+#include <utility>
+
+namespace bobopt {
+
+	// Implementation of make_unique based on paper N3656 by STL.
+	// http://isocpp.org/files/papers/N3656.txt
+	// Can't wait for C++14.
+
+	/// \brief Traits for make_unique called on single object.
+	template<typename T> struct unique_if
+	{
+		typedef std::unique_ptr<T> single_object;
+	};
+
+	/// \brief Traits for make_unique called on an array with uknown size.
+	template<typename T> struct unique_if<T[]>
+	{
+		typedef std::unique_ptr<T[]> unknown_bound;
+	};
+
+	/// \brief Traits for make_unique called on an array with known size.
+	template<typename T, size_t N> struct unique_if<T[N]>
+	{
+		typedef void known_bound;
+	};
+
+	/// \brief Definition for call on single object.
+	/// Just forwards argument to constructor of object.
+	template<typename T, typename... Args>
+	typename unique_if<T>::single_object make_unique(Args&&... args)
+	{
+		return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+	}
+
+	/// \brief Definition for call on an array with uknown size.
+	/// Default construct n objects of array element type.
+	template<typename T>
+	typename unique_if<T>::unknown_bound make_unique(size_t n)
+	{
+		typedef typename std::remove_extent<T>::type U;
+		return std::unique_ptr<T>(new U[n]());
+	}
+
+	/// \brief Delete definition for call on an array of known size.
+	template<typename T, typename... Args>
+	typename unique_if<T>::known_bound make_unique(Args&&...) BOBOPT_SPECIAL_DELETE;
+
+} // namespace
+
+#endif // guard
--- methods/bobopt_yield_complex.hpp
+++ methods/bobopt_yield_complex.hpp
@@ -4,15 +4,25 @@
 #include <bobopt_method.hpp>
 
 #include <clang/bobopt_clang_prolog.hpp>
+#include "clang/AST/ASTTypeTraits.h"
 #include "clang/Tooling/Refactoring.h"
 #include <clang/bobopt_clang_epilog.hpp>
 
+#include <memory>
 #include <string>
+#include <vector>
 
 // forward declarations:
 namespace clang {
 	class CXXRecordDecl;
 	class CXXMethodDecl;
+	class CompoundStmt;
+	class Stmt;
+	class IfStmt;
+	class ForStmt;
+	class WhileStmt;
+	class CompoundStmt;
+	class CXXTryStmt;
 }
 
 namespace bobopt {
@@ -39,6 +49,16 @@ namespace bobopt {
 				std::string parent_name;
 			};
 
+			struct complexity_tree_node;
+			typedef std::unique_ptr<complexity_tree_node> complexity_tree_node_pointer;
+
+			struct complexity_tree_node
+			{
+				clang::ast_type_traits::DynTypedNode node;
+				size_t complexity;
+				std::vector<complexity_tree_node_pointer> children;
+			};
+
 			// typedefs:
 			typedef clang::CXXMethodDecl* exec_function_type;
 
@@ -46,11 +66,26 @@ namespace bobopt {
 			void optimize_methods();
 			void optimize_method(exec_function_type method);
 
+			size_t analyze_for(clang::Stmt* init_stmt, clang::Stmt* cond_stmt) const;
+
+			complexity_tree_node_pointer create_if(clang::IfStmt* if_stmt) const;
+			complexity_tree_node_pointer create_for(clang::ForStmt* for_stmt) const;
+			complexity_tree_node_pointer create_while(clang::WhileStmt* while_stmt) const;
+			complexity_tree_node_pointer create_switch(clang::SwitchStmt* switch_stmt) const;
+			complexity_tree_node_pointer create_try(clang::CXXTryStmt* try_stmt) const;
+
+			complexity_tree_node_pointer search_and_create_call(clang::Stmt* stmt) const;
+
+			complexity_tree_node_pointer build_complexity_tree(clang::CompoundStmt* compound_stmt) const;
+			void insert_yields(const complexity_tree_node_pointer& root) const;
+
 			// data members:
 			clang::CXXRecordDecl* box_;
 			clang::tooling::Replacements* replacements_;
 
 			// constants.
+			static const size_t FOR_UNKNOWN_LOOP_COUNT = static_cast<size_t>(-1);
+			static const size_t FOR_RUNTIME_ANALYSIS_TRESHOLD = 50;
 			static const size_t BOX_EXEC_METHOD_COUNT = 3;
 			static const method_override BOX_EXEC_METHOD_OVERRIDES[BOX_EXEC_METHOD_COUNT];
 		};
