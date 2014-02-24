c698e4866aefcf88ba417b9596df02f553e93933
menjek <menjek@gmail.com>
2014-02-25 00:58:47 +0100

    Warning-less generated project with MSVC. Fixed library search and clang diagnostic.

--- clang/bobopt_clang_prolog.hpp
+++ clang/bobopt_clang_prolog.hpp
@@ -4,6 +4,7 @@
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
 #pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-parameter"
 
 #pragma warning(push)
 #pragma warning(disable : 4100 4127 4146 4217 4244 4245 4267 4291 4345 4510 4512 4610 4702 4800 4996)
