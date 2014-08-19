3d743ac7ce4cf37a27f57df46a4e3223ef7b7eae
menjek <menjek@gmail.com>
2014-08-19 22:02:15 +0200

    The number of boxes in prefetch benchmarks increased to 10. To correspond with thesis text.

--- benchmarks/prefetch/bench_prefetch.hpp
+++ benchmarks/prefetch/bench_prefetch.hpp
@@ -51,7 +51,7 @@ namespace bobopt
         typedef generic_model<distribute_box, bobox::BST_STATELESS> model;
 
         BOBOX_BOX_INPUTS_LIST(main, 0);
-        BOBOX_BOX_OUTPUTS_LIST(out0, 0, out1, 1, out2, 2, out3, 3, out4, 4, out5, 5, out6, 6, out7, 7, next, 8);
+        BOBOX_BOX_OUTPUTS_LIST(out0, 0, out1, 1, out2, 2, out3, 3, out4, 4, out5, 5, out6, 6, out7, 7, out8, 8, out9, 9, next, 10);
 
         distribute_box(const box_parameters_pack& box_params)
             : bobox::basic_box(box_params)
@@ -78,6 +78,8 @@ namespace bobopt
                 send_poisoned(outputs::out5());
                 send_poisoned(outputs::out6());
                 send_poisoned(outputs::out7());
+                send_poisoned(outputs::out8());
+                send_poisoned(outputs::out9());
                 send_poisoned(outputs::next());
                 return;
             }
@@ -94,6 +96,8 @@ namespace bobopt
             bench_send_envelope(this, outputs::out5(), in);
             bench_send_envelope(this, outputs::out6(), in);
             bench_send_envelope(this, outputs::out7(), in);
+            bench_send_envelope(this, outputs::out8(), in);
+            bench_send_envelope(this, outputs::out9(), in);
             bench_send_envelope(this, outputs::next(), in);
         }
     };
@@ -104,7 +108,7 @@ namespace bobopt
         typedef generic_model<last_distribute_box, bobox::BST_STATELESS> model;
 
         BOBOX_BOX_INPUTS_LIST(main, 0);
-        BOBOX_BOX_OUTPUTS_LIST(out0, 0, out1, 1, out2, 2, out3, 3, out4, 4, out5, 5, out6, 6, out7, 7);
+        BOBOX_BOX_OUTPUTS_LIST(out0, 0, out1, 1, out2, 2, out3, 3, out4, 4, out5, 5, out6, 6, out7, 7, out8, 8, out9, 9);
 
         last_distribute_box(const box_parameters_pack& box_params)
             : bobox::basic_box(box_params)
@@ -131,6 +135,8 @@ namespace bobopt
                 send_poisoned(outputs::out5());
                 send_poisoned(outputs::out6());
                 send_poisoned(outputs::out7());
+                send_poisoned(outputs::out8());
+                send_poisoned(outputs::out9());
                 return;
             }
 
@@ -146,6 +152,8 @@ namespace bobopt
             bench_send_envelope(this, outputs::out5(), in);
             bench_send_envelope(this, outputs::out6(), in);
             bench_send_envelope(this, outputs::out7(), in);
+            bench_send_envelope(this, outputs::out8(), in);
+            bench_send_envelope(this, outputs::out9(), in);
         }
     };
 
@@ -154,7 +162,7 @@ namespace bobopt
     public:
         typedef generic_model<collect_box, bobox::BST_STATELESS> model;
 
-        BOBOX_BOX_INPUTS_LIST(in0, 0, in1, 1, in2, 2, in3, 3, in4, 4, in5, 5, in6, 6, in7, 7);
+        BOBOX_BOX_INPUTS_LIST(in0, 0, in1, 1, in2, 2, in3, 3, in4, 4, in5, 5, in6, 6, in7, 7, in8, 8, in9, 9);
         BOBOX_BOX_OUTPUTS_LIST(main, 0);
 
         collect_box(const box_parameters_pack& box_params)
@@ -179,9 +187,11 @@ namespace bobopt
             auto env_in5 = pop_envelope(inputs::in5());
             auto env_in6 = pop_envelope(inputs::in6());
             auto env_in7 = pop_envelope(inputs::in7());
+            auto env_in8 = pop_envelope(inputs::in8());
+            auto env_in9 = pop_envelope(inputs::in9());
 
             if (env_in0->is_poisoned() || env_in1->is_poisoned() || env_in2->is_poisoned() || env_in3->is_poisoned() || env_in4->is_poisoned() ||
-                env_in5->is_poisoned() || env_in6->is_poisoned() || env_in7->is_poisoned())
+                env_in5->is_poisoned() || env_in6->is_poisoned() || env_in7->is_poisoned() || env_in8->is_poisoned() || env_in9->is_poisoned())
             {
                 send_poisoned(outputs::main());
                 return;
@@ -190,15 +200,17 @@ namespace bobopt
             BENCH_LOG_MEMFUNC_MSG("data collected.");
 
             auto in0 = *(env_in0->get_column(column_index_type(0)).get_data<unsigned>());
-            auto in1 = *(env_in0->get_column(column_index_type(0)).get_data<unsigned>());
-            auto in2 = *(env_in0->get_column(column_index_type(0)).get_data<unsigned>());
-            auto in3 = *(env_in0->get_column(column_index_type(0)).get_data<unsigned>());
-            auto in4 = *(env_in0->get_column(column_index_type(0)).get_data<unsigned>());
-            auto in5 = *(env_in0->get_column(column_index_type(0)).get_data<unsigned>());
-            auto in6 = *(env_in0->get_column(column_index_type(0)).get_data<unsigned>());
-            auto in7 = *(env_in0->get_column(column_index_type(0)).get_data<unsigned>());
+            auto in1 = *(env_in1->get_column(column_index_type(0)).get_data<unsigned>());
+            auto in2 = *(env_in2->get_column(column_index_type(0)).get_data<unsigned>());
+            auto in3 = *(env_in3->get_column(column_index_type(0)).get_data<unsigned>());
+            auto in4 = *(env_in4->get_column(column_index_type(0)).get_data<unsigned>());
+            auto in5 = *(env_in5->get_column(column_index_type(0)).get_data<unsigned>());
+            auto in6 = *(env_in6->get_column(column_index_type(0)).get_data<unsigned>());
+            auto in7 = *(env_in7->get_column(column_index_type(0)).get_data<unsigned>());
+            auto in8 = *(env_in8->get_column(column_index_type(0)).get_data<unsigned>());
+            auto in9 = *(env_in9->get_column(column_index_type(0)).get_data<unsigned>());
 
-            bench_send_envelope(this, outputs::main(), in0 + in1 + in2 + in3 + in4 + in5 + in6 + in7);
+            bench_send_envelope(this, outputs::main(), in0 + in1 + in2 + in3 + in4 + in5 + in6 + in7 + in8 + in9);
         }
     };
 
@@ -207,7 +219,7 @@ namespace bobopt
     public:
         typedef generic_model<sink_box, bobox::BST_STATELESS> model;
 
-        BOBOX_BOX_INPUTS_LIST(in0, 0, in1, 1, in2, 2, in3, 3, in4, 4, in5, 5, in6, 6, in7, 7);
+        BOBOX_BOX_INPUTS_LIST(in0, 0, in1, 1, in2, 2, in3, 3, in4, 4, in5, 5, in6, 6, in7, 7, in8, 8, in9, 9);
         BOBOX_BOX_OUTPUTS_LIST(main, 0);
 
         sink_box(const box_parameters_pack& box_params)
@@ -233,11 +245,13 @@ namespace bobopt
             auto env_in5 = pop_envelope(inputs::in5());
             auto env_in6 = pop_envelope(inputs::in6());
             auto env_in7 = pop_envelope(inputs::in7());
+            auto env_in8 = pop_envelope(inputs::in8());
+            auto env_in9 = pop_envelope(inputs::in9());
 
             BENCH_LOG_MEMFUNC_MSG("data in the sink.");
 
             if (env_in0->is_poisoned() || env_in1->is_poisoned() || env_in2->is_poisoned() || env_in3->is_poisoned() || env_in4->is_poisoned() ||
-                env_in5->is_poisoned() || env_in6->is_poisoned() || env_in7->is_poisoned())
+                env_in5->is_poisoned() || env_in6->is_poisoned() || env_in7->is_poisoned() || env_in8->is_poisoned() || env_in9->is_poisoned())
             {
                 send_poisoned(outputs::main());
                 return;
